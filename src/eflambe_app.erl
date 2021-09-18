19ae83914687e1b1ffa502530a832e98f684ba62
Trevor Brown <admin@stratus3d.com>
2021-09-17 20:07:51 -0400

    Run rebar3 new app eflambe

---
+++ src/eflambe_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc eflambe public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(eflambe_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    eflambe_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
