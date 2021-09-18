07367be4381184d44bbc2fad1e5d273c952f7933
Trevor Brown <admin@stratus3d.com>
2021-09-18 11:16:26 -0400

    Fix bug in eflambe_server
    Also add `running` boolean field to trace record

--- src/eflambe_server.erl
+++ src/eflambe_server.erl
@@ -29,6 +29,7 @@
           id :: any(),
           max_calls :: integer(),
           calls :: integer(),
+          running = false :: boolean(),
           options = [] :: list()
          }).
 
@@ -75,41 +76,40 @@ init([]) ->
                                   {stop, Reason :: any(), Reply :: any(), state()} |
                                   {stop, Reason :: any(), state()}.
 
-%-record(trace, {
-%          id :: any(),
-%          max_calls :: integer(),
-%          calls :: integer(),
-%          options = [] :: list()
-%         }).
-
 handle_call({start_trace, Id, MaxCalls, Options}, _From, State) ->
     case get_trace_by_id(State, Id) of
         undefined ->
             % Create new trace
-            NewState = put_trace(State, #trace{id = Id, max_calls = MaxCalls, calls = 0, options = Options}),
+            NewState = put_trace(State, #trace{id = Id, max_calls = MaxCalls, calls = 0, options = Options, running = true}),
             {reply, {ok, Id}, NewState};
-        #trace{max_calls = MaxCalls, calls = Calls, options = Options} ->
+        #trace{max_calls = MaxCalls, calls = Calls, options = Options, running = false} = Trace ->
             % Increment existing trace
             NewCalls = Calls + 1,
             case NewCalls =:= MaxCalls of
                 true ->
                     % End trace
-                    NewState = update_calls(State, Id, NewCalls),
+                    NewState = update_trace(State, Id, Trace#trace{calls = NewCalls, running = true}),
                     {reply, {end_trace, Id, NewCalls, Options}, NewState};
                 false ->
                     % Update number of calls
-                    NewState = update_calls(State, Id, NewCalls),
+                    NewState = update_trace(State, Id, Trace#trace{calls = NewCalls, running = true}),
                     {reply, {ok, Id}, NewState}
-            end
+            end;
+        #trace{max_calls = MaxCalls, options = Options, running = true} ->
+            {reply, {ok, Id}, State}
     end;
 
 handle_call({stop_trace, Id}, _From, State) ->
-    Reply = case get_trace_by_id(State, Id) of
-                undefined -> {error, unknown_trace};
-                #trace{calls = Calls, options = Options} ->
-                    {ok, Id, Calls, Options}
-            end,
-    {reply, Reply, State};
+    case get_trace_by_id(State, Id) of
+        undefined ->
+            {reply, {error, unknown_trace}, State};
+        #trace{calls = Calls, options = Options, running = true} = Trace ->
+            NewState = update_trace(State, Id, Trace#trace{running = false}),
+            Reply = {ok, Id, Calls, Options},
+            {reply, Reply, NewState};
+        #trace{calls = Calls, options = Options, running = false} ->
+            {reply, {ok, Id, Calls, Options}, State}
+    end;
 
 handle_call(_Request, _From, State) ->
     Reply = ok,
@@ -148,10 +148,9 @@ get_trace_by_id(#state{traces = Traces}, Id) ->
 put_trace(#state{traces = ExistingTraces} = State, NewTrace) ->
     State#state{traces = [NewTrace|ExistingTraces]}.
 
-update_calls(#state{traces = ExistingTraces} = State, Id, NewCalls) ->
-    {[Trace], Rest} = lists:partition(lookup_fun(Id), ExistingTraces),
-    Updated = Trace#trace{calls = NewCalls},
-    State#state{traces = [Updated|Rest]}.
+update_trace(#state{traces = ExistingTraces} = State, Id, UpdatedTrace) ->
+    {[_Trace], Rest} = lists:partition(lookup_fun(Id), ExistingTraces),
+    State#state{traces = [UpdatedTrace|Rest]}.
 
 lookup_fun(Id) ->
     fun
--- test/eflambe_server_SUITE.erl
+++ test/eflambe_server_SUITE.erl
@@ -97,11 +97,15 @@ start_trace(_Config) ->
     {ok, foobar} = eflambe_server:start_trace(foobar, 1, []),
 
     % Stores trace state
-    {state,[{trace,foobar,1,0,[]}]} = get_gen_server_state(eflambe_server),
+    {state,[{trace,foobar,1,0,true,[]}]} = get_gen_server_state(eflambe_server),
+
+    % Returns the same trace data when called twice
+    {ok, foobar} = eflambe_server:start_trace(foobar, 1, []),
 
     % Returns end_trace when max number of calls is reached
+    {ok, foobar, 0, []} = eflambe_server:stop_trace(foobar),
     {end_trace,foobar,1,[]} = eflambe_server:start_trace(foobar, 1, []),
-    {state,[{trace,foobar,1,1,[]}]} = get_gen_server_state(eflambe_server).
+    {state,[{trace,foobar,1,1,true,[]}]} = get_gen_server_state(eflambe_server).
 
 stop_trace(_Config) ->
     % Returns an error when eflambe_server isn't running
@@ -113,9 +117,10 @@ stop_trace(_Config) ->
 
     % Returns an ok tuple when eflambe_server is running and arguments are valid
     {ok, foobar} = eflambe_server:start_trace(foobar, 1, []),
+    {ok,foobar,0,[]} = eflambe_server:stop_trace(foobar),
 
     % Updates trace state on the server
-    {state,[{trace,foobar,1,0,[]}]} = get_gen_server_state(eflambe_server).
+    {state,[{trace,foobar,1,0,false,[]}]} = get_gen_server_state(eflambe_server).
 
 get_gen_server_state(Name) ->
     {status, _, _, State} = sys:get_status(Name),
