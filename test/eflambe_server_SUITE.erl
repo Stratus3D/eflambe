95a56f5c28e8b329857f966afb29a87e88c210d8
Trevor Brown <admin@stratus3d.com>
2021-10-05 17:44:56 -0400

    Refactor get_gen_server_state/1 function in tests

--- test/eflambe_server_SUITE.erl
+++ test/eflambe_server_SUITE.erl
@@ -35,7 +35,7 @@ all() ->
     ].
 
 suite() ->
-    [{ct_hooks,[cth_surefire]}, {timetrap, {seconds, 30}}].
+    [{ct_hooks, [cth_surefire]}, {timetrap, {seconds, 30}}].
 
 groups() ->
     [].
@@ -85,31 +85,31 @@ start_trace(_Config) ->
     Options = [{output_format, plain}],
 
     % Returns an error when eflambe_server isn't running
-    {'EXIT', {noproc, {gen_server,call,_}}} = (catch eflambe_server:start_trace(foobar, 1, Options)),
+    {'EXIT', {noproc, {gen_server, call, _}}} = (catch eflambe_server:start_trace(foobar, 1, Options)),
 
     % Returns an ok tuple when eflambe_server is running and arguments are valid
     {ok, _Pid} = eflambe_server:start_link(),
-    {state,[]} = get_gen_server_state(eflambe_server),
+    [] = get_gen_server_state(eflambe_server),
     {ok, foobar, true, TracerPid} = eflambe_server:start_trace(foobar, 1, Options),
 
     % Stores trace state
-    {state,[{trace,foobar,1,1,true,TracerPid,Options}]} = get_gen_server_state(eflambe_server),
+    [{trace, foobar, 1, 1, true, TracerPid, Options}] = get_gen_server_state(eflambe_server),
 
     % Returns the same trace data when called twice
     {ok, foobar, false, _TracerPid} = eflambe_server:start_trace(foobar, 1, Options),
-    {state,[{trace,foobar,1,1,true,TracerPid,Options}]} = get_gen_server_state(eflambe_server),
+    [{trace, foobar, 1, 1, true, TracerPid, Options}] = get_gen_server_state(eflambe_server),
 
     % Returns false when trace is stopped but number of calls has already been reached
     {ok, true} = eflambe_server:stop_trace(foobar),
-    {ok,foobar,false, _TracerPid} = eflambe_server:start_trace(foobar, 1, Options),
+    {ok, foobar, false, _TracerPid} = eflambe_server:start_trace(foobar, 1, Options),
 
-    {state,[{trace,foobar,1,1,false,TracerPid,Options}]} = get_gen_server_state(eflambe_server).
+    [{trace, foobar, 1, 1, false, TracerPid, Options}] = get_gen_server_state(eflambe_server).
 
 stop_trace(_Config) ->
     Options = [{output_format, plain}],
 
     % Returns an error when eflambe_server isn't running
-    {'EXIT', {noproc, {gen_server,call,_}}} = (catch eflambe_server:stop_trace(foobar)),
+    {'EXIT', {noproc, {gen_server, call, _}}} = (catch eflambe_server:stop_trace(foobar)),
 
     % Returns an error tuple when server is running but trace does not exist
     {ok, _Pid} = eflambe_server:start_link(),
@@ -120,9 +120,9 @@ stop_trace(_Config) ->
     {ok, true} = eflambe_server:stop_trace(foobar),
 
     % Updates trace state on the server
-    {state,[{trace,foobar,1,1,false,TracerPid,Options}]} = get_gen_server_state(eflambe_server).
+    [{trace, foobar, 1, 1, false, TracerPid, Options}] = get_gen_server_state(eflambe_server).
 
 get_gen_server_state(Name) ->
     {status, _, _, State} = sys:get_status(Name),
-    [[_, _, {data, [{"State", ServerState}]}]|_] = lists:reverse(State),
+    [[_, _, {data, [{"State", {state, ServerState}}]}]|_] = lists:reverse(State),
     ServerState.
